d9eda5ea3b456a8855db42cb86553bce0c555724
PashaHatsune <39mkui@gmail.com>
2026-04-10 18:22:00 +0300

    Добавить: ответы через answer

--- src/mxuserbot/modules/core/help.py
+++ src/mxuserbot/modules/core/help.py
@@ -1,6 +1,8 @@
-from ...core import loader
 from mautrix.types import MessageEvent
-from typing import Any  # Для правильной типизации mx, если не хочешь импортировать MXUserBot
+
+from ...core import loader
+from ...core import utils
+
 
 @loader.tds
 class MatrixModule(loader.Module):
@@ -17,14 +19,16 @@ class MatrixModule(loader.Module):
     }
 
     @loader.command()
-    async def help(self, mx: Any, event: MessageEvent):
+    async def help(self, mx, event: MessageEvent):
         """Отображает список команд"""
-        if not event.content.body:
-            return
+        
+        if self._is_core:
+            await self._db.get(owner="core", key="prefix")
 
         parts = event.content.body.split()
         args = parts[1] if len(parts) > 1 else None
-        prefix = mx.prefixes[0]
+        
+        prefix = await mx.get_prefix()
 
         if not args:
             msg = self.strings["header"].format(
@@ -34,25 +38,22 @@ class MatrixModule(loader.Module):
             msg += self.strings["modules_title"]
 
             for mod in mx.active_modules.values():
-                if mod.commands:
+                if hasattr(mod, "commands") and mod.commands:
                     cmds = ", ".join([f"<code>{prefix}{c}</code>" for c in mod.commands.keys()])
                 else:
                     cmds = self.strings["no_cmds"]
 
                 msg += self.strings["module_item"].format(
                     name=mod.friendly_name,
-                    desc=mod._help(),
+                    desc=mod._help() if hasattr(mod, "_help") else self.strings["no_desc"],
                     commands=cmds
                 )
 
-            return await mx.client.send_text(
-                room_id=event.room_id,
-                html=msg
-            )
+            return await utils.answer(mx, event.room_id, msg)
 
         cmd_name = args.lower()
         for mod in mx.active_modules.values():
-            if cmd_name in mod.commands:
+            if hasattr(mod, "commands") and cmd_name in mod.commands:
                 func = mod.commands[cmd_name]
                 doc = mod.strings.get(f"_cmd_doc_{cmd_name}") or func.__doc__ or self.strings["no_desc"]
 
@@ -61,11 +62,12 @@ class MatrixModule(loader.Module):
                     name=cmd_name,
                     desc=doc
                 )
-                return await mx.client.send_text(event.room_id, html=res)
+                return await utils.answer(mx, event.room_id, res)
 
-        await mx.client.send_text(
-            event.room_id,
-            html=self.strings["cmd_not_found"].format(
+        await utils.answer(
+            mx,
+            event.room_id, 
+            self.strings["cmd_not_found"].format(
                 prefix=prefix,
                 name=cmd_name
             )
